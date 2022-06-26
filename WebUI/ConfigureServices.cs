5f99549da15aa584cd93fff7e2dc657974829b62
Søren Lund <lund14@gmail.com>
2022-06-26 14:17:07 +0200

    Create template project

---
+++ WebUI/ConfigureServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebUI.Areas.Identity;
+using WebUI.Data;
+
+namespace WebUI;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddWebUIServices(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDatabaseDeveloperPageExceptionFilter();
+        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+        services.AddSingleton<WeatherForecastService>();
+
+        return services;
+    }
+}
---
+++ WebUI/Program.cs
@@ -0,0 +1,38 @@
+using WebUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddWebUIServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
+// Make the implicit Program class public so test projects can access it
+public partial class Program { }
